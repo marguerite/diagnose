bf883cffbbbd243138e28ac33f2b2b07d858fd85
marguerite <i@marguerite.su>
2020-11-02 16:05:51 +0800

    zypp repository

---
+++ zypp/repository/repository.go
@@ -0,0 +1,114 @@
+package repository
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+const (
+	PATH = "/etc/zypp/repos.d"
+)
+
+type Repositories []Repository
+
+type Repository struct {
+	File         string
+	Name         string
+	Enabled      bool
+	AutoRefresh  bool
+	BaseUrl      string
+	Type         string
+	Path         string
+	Priority     int
+	KeepPackages bool
+}
+
+func (r *Repository) Unmarshal(rd io.Reader) {
+	scanner := bufio.NewScanner(rd)
+	for scanner.Scan() {
+		if !strings.Contains(scanner.Text(), "=") {
+			replacer := strings.NewReplacer("[", "", "]", "")
+			r.Name = replacer.Replace(scanner.Text())
+			continue
+		}
+		arr := strings.Split(scanner.Text(), "=")
+		switch arr[0] {
+		case "enabled":
+			switch arr[1] {
+			case "0":
+				r.Enabled = false
+			default:
+				r.Enabled = true
+			}
+		case "autorefresh":
+			switch arr[1] {
+			case "0":
+				r.AutoRefresh = false
+			default:
+				r.AutoRefresh = true
+			}
+		case "keeppackages":
+			switch arr[1] {
+			case "0":
+				r.KeepPackages = false
+			default:
+				r.KeepPackages = true
+			}
+		case "baseurl":
+			r.BaseUrl = arr[1]
+		case "type":
+			r.Type = arr[1]
+		case "name":
+			r.Name = arr[1]
+		case "path":
+			r.Path = arr[1]
+		case "priority":
+			r.Priority, _ = strconv.Atoi(arr[1])
+		}
+	}
+	if r.Priority == 0 {
+		r.Priority = 99
+	}
+}
+
+func New() (repos Repositories) {
+	f, err := os.Open(PATH)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	files, err := f.Readdir(-1)
+	if err != nil {
+		panic(err)
+	}
+	for _, v := range files {
+		path := filepath.Join(PATH, v.Name())
+		repo, err := os.Open(path)
+		if err != nil {
+			repo.Close()
+			panic(err)
+		}
+		b := make([]byte, v.Size())
+		n, err := repo.Read(b)
+		if n != int(v.Size()) && n != 0 {
+			fmt.Printf("%s not fully read\n", v.Name())
+			os.Exit(1)
+		}
+		if err != io.EOF && err != nil {
+			panic(err)
+		}
+		repo.Close()
+
+		var r Repository
+		r.File = path
+		r.Unmarshal(bytes.NewReader(b))
+		repos = append(repos, r)
+	}
+	return repos
+}
